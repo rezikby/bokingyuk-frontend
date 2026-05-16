0837d39c5c2fb086d3030453db859dd85529795f
rezi <emailko@gmail.com>
2026-05-16 18:50:56 +0700

    fix production api url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL ||  'https://rezi.yopaaa.xyz/api';;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ||  'https://rezi.yopaaa.xyz/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
